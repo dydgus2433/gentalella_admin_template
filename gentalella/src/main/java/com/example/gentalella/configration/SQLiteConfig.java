e533c25cb6816c1b1160475d2ad4bc7c2fbed9dc
LeeYH <dydgus2433@naver.com>
2018-08-31 19:17:09 +0900

    DB config

---
+++ gentalella/src/main/java/com/example/gentalella/configration/SQLiteConfig.java
@@ -0,0 +1,19 @@
+package com.example.gentalella.configration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SQLiteConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+		dataSourceBuilder.url("jdbc:sqlite:test.db");
+		return dataSourceBuilder.build();
+	}
+}
